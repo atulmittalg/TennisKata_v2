4baf69c4a190ca34cca410e6517de70ddd41f2e4
Atul Mittal <55511902+atulmittalg@users.noreply.github.com>
2020-03-11 18:37:11 +0100

    Game score set to Love All at the start of the game

--- src/main/java/com/kata/tennis/TennisGame.java
+++ src/main/java/com/kata/tennis/TennisGame.java
@@ -1,4 +1,7 @@
 package com.kata.tennis;
 
 public class TennisGame {
+    public String getScore() {
+        return "Love All";
+    }
 }
--- src/test/java/com/kata/tennis/TennisGameTest.java
+++ src/test/java/com/kata/tennis/TennisGameTest.java
@@ -1,4 +1,12 @@
 package com.kata.tennis;
 
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class TennisGameTest {
+    @Test
+    public void getScoreShouldReturnLoveAllAtTheStartOfGame(){
+        TennisGame tennisGame = new TennisGame();
+        assertEquals("Love All", tennisGame.getScore());
+    }
 }
